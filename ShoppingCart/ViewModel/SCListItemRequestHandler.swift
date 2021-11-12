9c95caf9510e0a827ae47a3f650d965210b27987
fanwar-onshape <fanwar@ptc.com>
2021-11-12 11:09:25 +0530

    Separated parsing method. Added test cases class.

--- ShoppingCart/ViewModel/SCListItemRequestHandler.swift
+++ ShoppingCart/ViewModel/SCListItemRequestHandler.swift
@@ -11,9 +11,24 @@ import FZImageCache
 
 class SCListItemRequestHandler
 {
+    func parseResponse(results: [NSDictionary]) -> [SCItem]
+    {
+        var items = [SCItem]()
+        for result in results as [NSDictionary]
+        {
+            if let thumbnailSrtingArray = result.object(forKey: responseKeys.image_urls_thumbnails.rawValue) as? [String], let url = URL(string: thumbnailSrtingArray.first!), let thumbnailIdArray = result.object(forKey: responseKeys.image_ids.rawValue) as? [String], let image_id = thumbnailIdArray.first
+            {
+                let imageItem = Item(image: ImageCache.publicCache.placeholderImage, url: url, identifier: image_id)
+                
+                items.append(SCItem(created_at: result.object(forKey: responseKeys.createdAt.rawValue) as! String, price: result.object(forKey: responseKeys.price.rawValue) as! String, name: result.object(forKey: responseKeys.name.rawValue) as! String, uid: result.object(forKey: responseKeys.uid.rawValue) as! String, image_ids: result.object(forKey: responseKeys.image_ids.rawValue) as! [String], image_urls: result.object(forKey: responseKeys.image_urls.rawValue) as! [String], image_urls_thumbnails: result.object(forKey: responseKeys.image_urls_thumbnails.rawValue) as! [String], imageItem: imageItem))
+            }
+        }
+        return items
+    }
+    
     func fetchListItems(completion: @escaping ([SCItem]?) -> Void)
     {
-        AF.request(SCURLs.listItemURL.rawValue).responseJSON { response in
+        AF.request(SCURLs.listItemURL.rawValue).responseJSON { [self] response in
             switch response.result {
             case .success(let JSON):
                 print("Success with JSON: \(JSON)")
@@ -23,17 +38,14 @@ class SCListItemRequestHandler
                     completion(nil)
                     return
                 }
-                var items = [SCItem]()
-                for result in results as [NSDictionary]
+                let items = parseResponse(results: results)
+                if items.count > 0
                 {
-                    if let thumbnailSrtingArray = result.object(forKey: responseKeys.image_urls_thumbnails.rawValue) as? [String], let url = URL(string: thumbnailSrtingArray.first!), let thumbnailIdArray = result.object(forKey: responseKeys.image_ids.rawValue) as? [String], let image_id = thumbnailIdArray.first
-                    {
-                        let imageItem = Item(image: ImageCache.publicCache.placeholderImage, url: url, identifier: image_id)
-                        
-                        items.append(SCItem(created_at: result.object(forKey: responseKeys.createdAt.rawValue) as! String, price: result.object(forKey: responseKeys.price.rawValue) as! String, name: result.object(forKey: responseKeys.name.rawValue) as! String, uid: result.object(forKey: responseKeys.uid.rawValue) as! String, image_ids: result.object(forKey: responseKeys.image_ids.rawValue) as! [String], image_urls: result.object(forKey: responseKeys.image_urls.rawValue) as! [String], image_urls_thumbnails: result.object(forKey: responseKeys.image_urls_thumbnails.rawValue) as! [String], imageItem: imageItem))
-                    }
+                    completion(items)
+                } else {
+                    completion(nil)
                 }
-                completion(items)
+               
             case .failure(let error):
                 print("Failure with error: \(error)")
                 completion(nil)
