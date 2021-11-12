b33f952f6e833e904847a14ae8afc5ebcfd69f82
fanwar-onshape <fanwar@ptc.com>
2021-11-12 12:17:42 +0530

    Updated Detail View added created at label.

--- ShoppingCart/View/Detail View/SCItemDetailViewController.m
+++ ShoppingCart/View/Detail View/SCItemDetailViewController.m
@@ -14,6 +14,8 @@
 @property (weak, nonatomic) IBOutlet UIImageView *imageViewItem;
 @property (weak, nonatomic) IBOutlet UILabel *labelPriceTitle;
 @property (weak, nonatomic) IBOutlet UILabel *labelPrice;
+@property (weak, nonatomic) IBOutlet UILabel *labelCreatedAtTitle;
+@property (weak, nonatomic) IBOutlet UILabel *labelCreatedAtValue;
 
 @end
 
@@ -25,7 +27,9 @@
     // Do any additional setup after loading the view.
     self.title = item.name;
     self.labelPrice.text = item.price;
+    self.labelCreatedAtValue.text = item.created_at;
     self.labelPriceTitle.text = NSLocalizedString(@"Price_Title", comment: @"Title Text for Price Label");
+    self.labelCreatedAtTitle.text = NSLocalizedString(@"Created_At_Title", comment: "Title Text for Created at");
     NSURL *url = [[NSURL alloc] initWithString:item.images.firstObject.image_url];
     Item *imagesItem = [[Item alloc] initWithImage:ImageCache.publicCache.placeholderImage url:url identifier: item.images.firstObject.image_id];
     self.imageViewItem.image = imagesItem.image;
